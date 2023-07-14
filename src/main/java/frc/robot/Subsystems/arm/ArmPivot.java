108e3f894bf4458d35524c5ba24c12dd4609b7e8
Anemone <zombiegam3r2o06@gmail.com>
2023-07-14 11:41:01 -0700

    Pivot stuff

--- src/main/java/frc/robot/Subsystems/arm/ArmPivot.java
+++ src/main/java/frc/robot/Subsystems/arm/ArmPivot.java
@@ -1,11 +1,16 @@
 package frc.robot.Subsystems.arm;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.MotorIDs;
+import frc.robot.Libraries.Util.PIDConfig;
 import frc.robot.Libraries.Util.SparkMax.SparkMaxConfig;
 import frc.robot.Libraries.Util.SparkMax.SparkMaxEncoderType;
+import frc.robot.Libraries.Util.SparkMax.SparkMaxSetup;
 import frc.robot.Libraries.Util.SparkMax.SparkMaxStatusFrames;
 
 public class ArmPivot extends SubsystemBase {
@@ -38,11 +43,40 @@ public class ArmPivot extends SubsystemBase {
             true,
             0,
             false,
-            null,
-            null,
-            null,
-            0
-        );
+            new PIDConfig(6, 0.0, 1, 0)
+    );
+    private SparkMaxConfig pivotConfig2 = new SparkMaxConfig(
+        new SparkMaxStatusFrames(
+            100,
+            100,
+            500,
+            500,
+            500,
+            500,
+            500),
+            1000,
+            true,
+            IdleMode.kCoast,
+            30,
+            30,
+            true,
+            motor1
+    );
+
+    public ArmPivot() {
+        motor1 = new CANSparkMax(MotorIDs.ARM_PIVOT_ANGLE, MotorType.kBrushless);
+        SparkMaxSetup.setup(motor1, pivotConfig1);
+        motor2 = new CANSparkMax(MotorIDs.ARM_PIVOT_ANGLE_FOLLOWER, MotorType.kBrushless);
+        SparkMaxSetup.setup(motor2, pivotConfig2);
+    }
+
+    public void setAngle(double angle) {
+        motor1.getPIDController().setReference(angle, ControlType.kPosition);
+    }
+
+    public double getAngle() {
+        return motor1.getEncoder().getPosition();
+    }
 
 
 }
