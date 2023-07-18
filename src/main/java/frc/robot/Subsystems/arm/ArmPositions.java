00236f620e8f579e44a31f7cba392d93b9bb4901
Anemone <zombiegam3r2o06@gmail.com>
2023-07-17 17:56:52 -0700

    positions

--- src/main/java/frc/robot/Subsystems/arm/ArmPositions.java
+++ src/main/java/frc/robot/Subsystems/arm/ArmPositions.java
@@ -2,26 +2,26 @@ package frc.robot.Subsystems.arm;
 
 public enum ArmPositions {
     // TODO: all of this is probably bad now, bruh moment
-    AUTO_ALIGN(0, 0, 0),
-    STOW(0, 0, 0),
+    AUTO_ALIGN(0.785, 0, 0.35), //taken from original
+    STOW(.48, 0, .242), //taken from original
 
-    HALF_CONE_PLACE_HIGH(0,0,0),
-    PRE_CONE_PLACE_HIGH(0, 0, 0),
-    CONE_PLACE_HIGH(0,0,0),
+    HALF_CONE_PLACE_HIGH(0,0,0), //fill in the crossover
+    PRE_CONE_PLACE_HIGH(.785, 71, .567), //taken from original
+    CONE_PLACE_HIGH(.745, 71, .567), //same as prep but with caligirlsmovedownalittle incorporated
     
-    HALF_CONE_PLACE_MID(0,0,0),
-    PRE_CONE_PLACE_MID(0,0,0),
-    CONE_PLACE_MID(0,0,0),
+    HALF_CONE_PLACE_MID(0,0,0), //fill in the crossover
+    PRE_CONE_PLACE_MID(.77, 21.5, .619), //taken from original
+    CONE_PLACE_MID(.73, 21.5, .619), //same as prep but with caligirlsmovedownalittle incorporated
     
-    INTAKE_GROUND(0,0,0),
-    INTAKE_SUBSTATION(0,0,0), 
+    INTAKE_GROUND(.585, 31.4, 0.47),
+    INTAKE_SUBSTATION(.78, 0, .66), 
     
-    CUBE_PLACE_HIGH(0,0,0), 
+    CUBE_PLACE_HIGH(.77, 47, .65), 
     
-    CUBE_PLACE_MID(0,0,0),
+    CUBE_PLACE_MID(.642, 0, .493),
     
-    DRIVE_POSITION(0,0,0),
-    PRE_DRIVE_POSITION(0,0,0);
+    DRIVE_POSITION(.5,0,.31),
+    PRE_DRIVE_POSITION(.5,0,.45);
 
     public double armAngle, extension, wrist;
 
