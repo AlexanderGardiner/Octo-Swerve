7d9e83a46b879d49a455a77ba9522f2d2ae24fc3
Anemone <zombiegam3r2o06@gmail.com>
2023-08-24 14:05:39 -0700

    Command planning

--- src/main/java/frc/robot/ButtonConfig.java
+++ src/main/java/frc/robot/ButtonConfig.java
@@ -8,13 +8,26 @@ import frc.robot.Commands.arms.CubeHigh;
 import frc.robot.Commands.arms.CubeMid;
 
 public class ButtonConfig {
-    public static void initTeleop() { //these are wrong, please assign them properly. TODO: This will be redone later.
-        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 1).onTrue(new ConeHigh());
-        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 2).onTrue(new ConeMid());
-        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 3).onTrue(new CubeHigh());
-        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 4).onTrue(new CubeMid());
-        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 5).onTrue(new CollectFloor());
+    public static void initTeleop() {
+        //DRIVER 6 is gyro reset
+        //DRIVER 13 is driver assist, should be removed
 
+        //DRIVER_RIGHT 1 is auto align
 
+        //DRIVER_LEFT 1 is 0 degrees
+        //DRIVER_LEFT 2 is 180 degrees
+
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 1).onTrue(new CollectFloor());
+        //CODRIVER 2 is substation collect
+        //CODRIVER 3 is drive/stow both
+        //CODRIVER 4 is hippo intake
+        //CODRIVER 5 is hippo place
+        //CODRIVER 6 is arm zero
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 7).onTrue(new CubeHigh());
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 8).onTrue(new ConeHigh());
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 9).onTrue(new CubeMid());
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 10).onTrue(new ConeMid());
+        //CODRIVER 11 is arm auto align position
+        //CODRIVER 12 is stow both
     }
 }
