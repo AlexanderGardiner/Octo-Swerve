0ccf0c5fc61cf271e0fa583400639c02e0a691eb
Anemone <zombiegam3r2o06@gmail.com>
2023-08-31 11:27:52 -0700

    gyro zero coding practices

--- src/main/java/frc/robot/Commands/Swerve/GyroZero.java
+++ src/main/java/frc/robot/Commands/Swerve/GyroZero.java
@@ -1,19 +1,24 @@
 package frc.robot.Commands.Swerve;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Subsystems.drivetrain.SwerveDrive;
+import frc.robot.Subsystems.light.Animations;
+import frc.robot.Subsystems.light.Light;
 
 public class GyroZero extends InstantCommand{
 
 
+    private SwerveDrive swerveDrive;
+
+    public GyroZero() {
+        this.swerveDrive = SwerveDrive.getInstance();
+    }
     @Override
     public void initialize() {
-        SwerveDrive.getInstance().resetGyro();
-        SwerveDrive.getInstance().updatePose();
-        SwerveDrive.getInstance().setTargetPose2d(SwerveDrive.getInstance().getPose2d());
+        Light.getInstance().setAnimation(Animations.BOOT_COMPLETE);
+        swerveDrive.resetGyro();
+        swerveDrive.updatePose();
+        swerveDrive.setTargetPose2d(SwerveDrive.getInstance().getPose2d());
     }
     
 }
